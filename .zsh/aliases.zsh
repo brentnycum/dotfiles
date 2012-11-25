24df64e03f88d7e86eaa66ecd4dd7e358b7cf86a
Brent Nycum <brentnycum@gmail.com>
2012-11-24 20:04:21 -0600

    Adding subl alias to Sublime Text 2.

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -12,3 +12,5 @@ alias gs='git status'
 
 # Used to create a blank git repo to upload to my source control server
 alias blankgit='mkdir blank.git; cd blank.git; git --bare init; touch git-daemon-export-ok; git --bare update-server-info; mv hooks/post-update.sample hooks/post-update; cd ..'
+
+alias subl="/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl"
