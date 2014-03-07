af321c96bf5feb90d89798669354f2665cde79bc
Brent Nycum <brentnycum@gmail.com>
2014-03-07 09:35:52 -0600

    Fix PATH.

--- .zsh/config.zsh
+++ .zsh/config.zsh
@@ -2,7 +2,7 @@ export EDITOR='vim'
 export SVN_EDITOR='vim'
 export GIT_EDITOR='vim'
 
-export PATH="$HOME/.rbenv/bin:$PATH"
+export PATH="$HOME/.rbenv/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin"
 
 setopt autocd
 setopt correctall
